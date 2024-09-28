d276e00f11764ab40e07954b18fb11d089b8b648
Tempe Techie <95053628+tempe-techie@users.noreply.github.com>
2024-09-28 22:58:15 +0200

    landing page update

--- controllers/public.js
+++ controllers/public.js
@@ -1,7 +1,8 @@
 export function index(request, reply) {
-  // if subdomain (arb.delegate2.name) starts with "arb" then render index-arb.liquid, otherwise render index.liquid  
   if (request.headers.host.startsWith("arb.")) {
     return reply.view("./templates/index-arb.liquid");
+  } else if (request.headers.host.startsWith("op.")) {
+    return reply.view("./templates/index-op.liquid");
   }
 
   return reply.view("./templates/index.liquid");
